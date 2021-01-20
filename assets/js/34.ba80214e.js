6d0ced5cc18301ba77feab6e16eff30d8115e335
houxueping <houxueping@didiglobal.com>
2021-01-20 14:12:59 +0800

    deploy

---
+++ assets/js/34.ba80214e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{454:function(t,s,e){"use strict";e.r(s);var n=e(43),o=Object(n.a)({},(function(){var t=this.$createElement,s=this._self._c||t;return s("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[s("hr"),this._v(" "),s("myself"),this._v(" "),s("back-to-top")],1)}),[],!1,null,null,null);s.default=o.exports}}]);
---
+++ json/baseInfo.js
@@ -0,0 +1,14 @@
+export default [
+	{ name: '姓名', value: '侯雪平' },
+	{ name: '出生日期', value: '1993/04' },
+	{ name: '手机', value: '18701355361' },
+	{ name: '邮箱', value: '2640952808@qq.com' },
+	{ name: '民族', value: '汉' },
+	{ name: '现居', value: '北京市海淀区' },
+	{ name: '籍贯', value: '河北衡水' },
+	{ name: '工作年限', value: `${new Date().getFullYear() - 2016}年` },
+	{ name: '状态', value: '在职' },
+	{ name: '学校', value: '河北师范大学' },
+	{ name: '专业', value: '电子信息科学与技术' },
+	{ name: '是否统招', value: '是' },
+];
